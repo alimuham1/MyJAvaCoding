1c99b637f8dfacbca4bc7d35b0a5696c9319c6cf
alimuham1 <k102066@nu.edu.pk>
2021-05-13 13:42:40 +0200

    Update Main.java
    Create testing call in Main.java file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("This is my first line of code");
+	System.out.println("This is testing");    
     }
 }
